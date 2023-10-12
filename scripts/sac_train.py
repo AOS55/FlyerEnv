43e40b3e87edef933b3c7730f4b5dcf43d12955b
quessy <aq15777@bristol.ac.uk>
2023-10-12 16:49:55 +0100

    add basic sac_train script

--- scripts/sac_train.py
+++ scripts/sac_train.py
@@ -3,13 +3,19 @@ from stable_baselines3 import SAC
 
 
 def main(): 
-    env = gym.make("flyer-v1")
+
+
+    env_config = {
+        "area": (256, 256)
+    }
+    env = gym.make("trajectory-v1", config=env_config)
 
     model = SAC("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=1000, log_interval=4)
+    model.learn(total_timesteps=100000, log_interval=4, progress_bar=True)
 
     obs, info = env.reset()
-    while True:
+    done = False
+    while not done:
         action, _states = model.predict(obs, deterministic=True)
         obs, reward, terminated, truncated, info = env.step(action)
         if terminated or truncated:
