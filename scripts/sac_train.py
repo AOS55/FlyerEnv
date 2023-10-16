e0db3beff67230cd1339537aada16b89b6ccc493
quessy <aq15777@bristol.ac.uk>
2023-10-16 11:52:17 +0100

    changed to sl flight

--- scripts/sac_train.py
+++ scripts/sac_train.py
@@ -1,43 +1,59 @@
 import gymnasium as gym
 from stable_baselines3 import SAC
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.monitor import Monitor
 import wandb
 from wandb.integration.sb3 import WandbCallback
+
 
 def main(): 
 
 
     env_config = {
         "area": (256, 256),
-        "simulation_frequency": 100.0
+        "simulation_frequency": 100.0,
+        "duration": 10.0,
+        "trajectory_config": {
+            "name": "sl"
+        },
     }
 
     config = {
         "policy_type": "MlpPolicy",
-        "total_timesteps": 1000000,
+        "total_timesteps": 10000000,
         "env_name": "trajectory-v1",
         "env_config": env_config
     }
 
-    # env_id = "trajectory-v1"
-    # env = gym.make("trajectory-v1", config=env_config)
+    env = gym.make(config["env_name"], config=env_config)
+    # env = gym.make(config["env_name"])
 
     run = wandb.init(
         project="FlyerEnv-tests",
         config=config,
-        sync_tensorboard=False
+        sync_tensorboard=True
     )
 
-    env = make_vec_env(config["env_name"], n_envs=256, seed=0, env_kwargs={"config": env_config})
+    env = make_vec_env(config["env_name"], n_envs=32, seed=0, env_kwargs={"config": env_config})
+    eval_env = gym.make(config['env_name'], config=env_config)
+    eval_env = Monitor(eval_env)
+    # env = make_vec_env(config["env_name"], n_envs=32)
+    # env = gym.wrappers.RecordEpisodeStatistics(env)
 
-    model = SAC("MlpPolicy", env, verbose=1)
+    eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/",
+                             log_path="./logs/", eval_freq=500,
+                             deterministic=True, render=False)
+    
+
+    model = SAC("MlpPolicy", env, verbose=1, tensorboard_log=f".runs/sac")
     model.learn(total_timesteps=config["total_timesteps"],
                 log_interval=4,
                 progress_bar=True,
-                callback=WandbCallback(
+                callback=[WandbCallback(
                     model_save_path=f"models/{run.id}",
                     verbose=2
-                ))
+                ), eval_callback])
     run.finish()
     
     obs, info = env.reset()
