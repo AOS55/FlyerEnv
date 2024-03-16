63738ff1af7f283714850cfcdeef16999340f1ad
AOS55 <aq15777@bristol.ac.uk>
2024-03-16 19:19:29 +0000

    add relative assertations

--- tests/aircraft/test_trim.py
+++ tests/aircraft/test_trim.py
@@ -1,8 +1,9 @@
 import pytest
+import os
 
 from pyflyer import Aircraft
 
-aircraft_types = ["TO"]
+aircraft_types = ["TO"]  # Only setup for TO for now
 trim_results = {
     "TO": [0.0028701873057483977, 0.04046632028946647, 0.763808910861435]
 }
@@ -10,11 +11,12 @@ trim_results = {
 
 @pytest.mark.parametrize("aircraft_type", aircraft_types)
 def test_trim(aircraft_type):
-    aircraft = Aircraft(aircraft_type)
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data/")
+    aircraft = Aircraft(data_path = os.path.join(path))
     altitude = 1000.0  # trim altitude to maintain
     airspeed = 100.0  # trim airspeed to maintain
     n_steps = 100  # Number of PSO trim iterations
     trim = aircraft.trim(altitude, airspeed, n_steps)
-    assert trim[0] == pytest.approx(0)  # aileron trim position
-    assert trim[1] == pytest.approx(trim_results[aircraft_type][1])  # elevator trim position
-    assert trim[2] == pytest.approx(trim_results[aircraft_type][2])  # TLA trim position
+    assert trim[0] == pytest.approx(trim_results[aircraft_type][0], abs=1e-1)  # pitch attitude
+    assert trim[1] == pytest.approx(trim_results[aircraft_type][1], rel=1e-2)  # elevator trim position
+    assert trim[2] == pytest.approx(trim_results[aircraft_type][2], rel=1e-1)  # TLA trim position
