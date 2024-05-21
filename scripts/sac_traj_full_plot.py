27183433fbbc4a81e38c1bbc0c9b5755eee49e18
quessy <aq15777@bristol.ac.uk>
2024-05-21 11:02:25 +0100

    Change in plot functions

--- scripts/sac_traj_full_plot.py
+++ scripts/sac_traj_full_plot.py
@@ -18,18 +18,22 @@ def main():
 
     env_config = {
         "observation": {
-            "type": "Longitudinal"
+            "type": "Trajectory"
         },
         "action": {
-            "type": "LongitudinalAction"
+            "type": "ContinuousAction"
         },
         "area": (256, 256),
         "simulation_frequency": 100.0,
         "duration": 10.0,
+        "trajectory_config": {
+            "name": "sl",
+            "length": 10.0
+        }
     }
 
-    env = gym.make("control-v1", config=env_config)
-    policy = SAC.load('models/control_long-v1/best_model.zip')
+    env = gym.make("trajectory-v1", config=env_config)
+    policy = SAC.load('models/trajectory_full-v1/best_model.zip')
 
     control_env_config = {
         "observation": {
@@ -43,12 +47,12 @@ def main():
         "duration": 10.0
     }
 
-    # c_env = gym.make("control-v1", config=control_env_config)
-    # c_obs, c_info = c_env.reset()
-    # c_done = False
-    # c_action = [0.0, 1.0,
-    #     lmap(-1000.0, [0.0, -10000], [-1.0, 1.0]),
-    #     lmap(100.0, [60.0, 110.0], [-1.0, 1.0])]
+    c_env = gym.make("control-v1", config=control_env_config)
+    c_obs, c_info = c_env.reset()
+    c_done = False
+    c_action = [0.0, 1.0,
+        lmap(-1000.0, [0.0, -10000], [-1.0, 1.0]),
+        lmap(100.0, [60.0, 110.0], [-1.0, 1.0])]
     
     obs, info = env.reset()
     done = False
@@ -80,26 +84,26 @@ def main():
             'reward': reward
         }
 
-        # c_obs, c_reward, c_terminated, c_truncated, c_info = c_env.step(c_action)
+        c_obs, c_reward, c_terminated, c_truncated, c_info = c_env.step(c_action)
 
-        # c_v_dict = c_env.unwrapped.vehicle.dict
-        # c_controls = c_env.unwrapped.vehicle.controls
+        c_v_dict = c_env.unwrapped.vehicle.dict
+        c_controls = c_env.unwrapped.vehicle.controls
 
-        # c_obs_dict = {
-        #     'c_elevator': c_controls[1],
-        #     'c_aileron': c_controls[0],
-        #     'c_tla': c_controls[2],
-        #     'c_rudder': c_controls[3],
-        #     'c_x': c_v_dict['x'],
-        #     'c_y': c_v_dict['y'],
-        #     'c_z': c_v_dict['z'],
-        #     'c_pitch': c_v_dict['pitch'],
-        #     'c_roll': c_v_dict['roll'],
-        #     'c_yaw': c_v_dict['yaw'],
-        #     'c_u': c_v_dict['u'],
-        #     'c_reward': c_reward, 
-        # }
-        # obs_dict.update(c_obs_dict)
+        c_obs_dict = {
+            'c_elevator': c_controls[1],
+            'c_aileron': c_controls[0],
+            'c_tla': c_controls[2],
+            'c_rudder': c_controls[3],
+            'c_x': c_v_dict['x'],
+            'c_y': c_v_dict['y'],
+            'c_z': c_v_dict['z'],
+            'c_pitch': c_v_dict['pitch'],
+            'c_roll': c_v_dict['roll'],
+            'c_yaw': c_v_dict['yaw'],
+            'c_u': c_v_dict['u'],
+            'c_reward': c_reward, 
+        }
+        obs_dict.update(c_obs_dict)
 
         times.append(time)
         observations.append(obs_dict)
@@ -126,34 +130,34 @@ def plot_long(outputs, times, exp_len):
 
     ax[0].set_title(r"\textbf{Longitudinal Tracking}")
     ax[0].plot(times, outputs['elevator'], c=COLOURS[5], label=r'elevator')
-    # ax[0].plot(times, outputs['c_elevator'], linestyle='dashed' , c=COLOURS[5])
+    ax[0].plot(times, outputs['c_elevator'], linestyle='dashed' , c=COLOURS[5])
     ax[0].plot(times, outputs['tla'], c=COLOURS[6], label=r'tla')
-    # ax[0].plot(times, outputs['c_tla'], linestyle='dashed', c=COLOURS[6])
+    ax[0].plot(times, outputs['tla'], linestyle='dashed', c=COLOURS[6])
     ax[0].set_ylabel(r"$\delta [^{\circ}]$", fontsize=15)
     ax[0].legend(title=r'\textbf{Control}')
 
     ax[1].plot(times, outputs['pitch'] * 180.0 / np.pi, c=COLOURS[1])
-    # ax[1].plot(times, outputs['c_pitch'] * 180.0 / np.pi, linestyle='dashed', c=COLOURS[1])
+    ax[1].plot(times, outputs['c_pitch'] * 180.0 / np.pi, linestyle='dashed', c=COLOURS[1])
     ax[1].set_ylabel(r"$\theta [^{\circ}]$", fontsize=15)
 
     ax[2].plot(times, outputs['z'], c=COLOURS[1])
-    # ax[2].plot(times, outputs['c_z'], linestyle='dashed', c=COLOURS[1])
+    ax[2].plot(times, outputs['c_z'], linestyle='dashed', c=COLOURS[1])
     # ax[2].plot(times, outputs['z_com'], linestyle='dashed', c=COLOURS[2])
     ax[2].set_ylabel(r"$z [m]$", fontsize=15)
 
     ax[3].plot(times, outputs['u'], c=COLOURS[1])
-    # ax[3].plot(times, outputs['c_u'], linestyle='dashed', c=COLOURS[1])
+    ax[3].plot(times, outputs['c_u'], linestyle='dashed', c=COLOURS[1])
     ax[3].set_ylabel(r"$u [\frac{m}{s}]$", fontsize=15)
 
     ax[4].plot(times, outputs['reward'], c=COLOURS[1])
-    # ax[4].plot(times, outputs['c_reward'], linestyle='dashed', c=COLOURS[1])
+    ax[4].plot(times, outputs['c_reward'], linestyle='dashed', c=COLOURS[1])
     ax[4].set_ylabel(r'Reward', fontsize=15)
     ax[4].set_xlabel(r'time [$s$]', fontsize=15)
 
     [axis.set_xlim(0.0, exp_len) for axis in ax]
     [axis.xaxis.set_tick_params(labelsize=15) for axis in ax]
     [axis.yaxis.set_tick_params(labelsize=15) for axis in ax]
-    fig.savefig("long_control_long.pdf")
+    fig.savefig("traj_long.pdf")
 
 def plot_lat(outputs, times, exp_len):
 
@@ -180,10 +184,11 @@ def plot_lat(outputs, times, exp_len):
     ax[3].set_ylabel(r'Reward', fontsize=15)
     ax[3].set_xlabel(r'time [$s$]', fontsize=15)
 
+
     [axis.set_xlim(0.0, exp_len) for axis in ax]
     [axis.xaxis.set_tick_params(labelsize=15) for axis in ax]
     [axis.yaxis.set_tick_params(labelsize=15) for axis in ax]
-    fig.savefig("long_control_lat.pdf")
+    fig.savefig("traj_lat.pdf")
     fig.show()
 
 def plot_track(outputs):
